1b1756c50d86aef275028767464b49280a029122
Kyung Myung Ko <peanut61313@gmail.com>
2019-02-17 00:06:35 -0600

    Mocks and EFCores

---
+++ ReviewMyProduct.WebUI/Review.Data/Implementation/Mock/MockProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using Review.Data.Interfaces;
+using Review.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Review.Data.Implementation.Mock
+{
+    public class MockProductRepository : IProductRepository
+    {
+        private List<Product> Products = new List<Product>();
+
+        public Product Create(Product newProduct)
+        {
+            newProduct.Id = Products.OrderByDescending(p => p.Id).Single().Id + 1;
+            Products.Add(newProduct);
+
+            return newProduct;
+        }
+
+        public bool DeleteById(int productId)
+        {
+            var product = GetById(productId);
+            Products.Remove(product);
+
+            return true;
+        }
+
+        public ICollection<Product> GetByCommentId(int commentId)
+        {
+            return Products.FindAll(p => p.CommentId == commentId);
+        }
+
+        public Product GetById(int productId)
+        {
+            return Products.Single(p => p.Id == productId);
+        }
+
+        public Product Update(Product updatedProduct)
+        {
+            DeleteById(updatedProduct.Id);
+            Products.Add(updatedProduct);
+
+            return updatedProduct;
+        }
+    }
+}
---
+++ ReviewMyProduct.WebUI/Review.Data/Implementation/Mock/MockRatingRepository.cs
@@ -0,0 +1,30 @@
+﻿using Review.Data.Interfaces;
+using Review.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Review.Data.Implementation.Mock
+{
+
+    public class MockRatingRepository : IRatingRepository
+    {
+        private List<Rating> Ratings = new List<Rating>();
+
+        public ICollection<Rating> GetByCommentId(int commentId)
+        {
+            return Ratings.FindAll(r => r.CommentId == commentId);
+        }
+
+        public Rating GetById(int ratingId)
+        {
+            return Ratings.Single(r => r.Id == ratingId);
+        }
+
+        public ICollection<Rating> GetByUserId(int userId)
+        {
+            return Ratings.FindAll(r => r.UserId == userId);
+        }
+    }
+}
